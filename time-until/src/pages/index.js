873fa42f1a046b1dd77fe0d3ae94a17db8f600d9
bennypc <52959626+bennypc@users.noreply.github.com>
2023-06-25 02:33:21 -0700

    timer

--- time-until/src/pages/index.js
+++ time-until/src/pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Range } from "react-range";
 import moment from "moment";
 
@@ -6,6 +6,7 @@ export default function Home() {
   const [values, setValues] = useState([
     moment().hour() * 60 + moment().minutes()
   ]);
+  const [countdown, setCountdown] = useState("");
 
   const formatTime = (value) => {
     const hours = Math.floor(value / 60);
@@ -15,9 +16,29 @@ export default function Home() {
       .padStart(2, "0")}`;
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = moment();
+      const selectedTime = moment(formatTime(values[0]), "HH:mm");
+      const duration = moment.duration(selectedTime.diff(now));
+
+      if (duration.asSeconds() < 0) {
+        setCountdown("Time has passed!");
+      } else {
+        setCountdown(
+          `${duration.hours()} hours ${duration.minutes()} minutes ${duration.seconds()} seconds`
+        );
+      }
+    }, 100); // Here we update the countdown every 100 milliseconds
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(interval);
+  }, [values]);
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-600">
-      <div className="w-1/2 py-4 bg-white rounded shadow">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-600">
+      <h1 className="text-3xl mb-8 text-white">TIME UNTIL</h1>
+      <div className="mx-2 md:w-1/2 py-4 bg-white rounded shadow">
         <Range
           step={1}
           min={0}
@@ -25,20 +46,21 @@ export default function Home() {
           values={values}
           onChange={(values) => setValues(values)}
           renderTrack={({ props, children }) => (
-            <div {...props} className="h-1 bg-blue-300 rounded cursor-pointer">
+            <div {...props} className="h-1 bg-black rounded cursor-pointer">
               {children}
             </div>
           )}
           renderThumb={({ props }) => (
             <div
               {...props}
-              className="w-6 h-6 bg-blue-600 rounded-full focus:outline-none"
+              className="w-6 h-6 bg-red-600 rounded-full focus:outline-none"
             />
           )}
         />
         <div className="text-center mt-4 text-lg">
-          Selected time: {formatTime(values[0])}
+          SELECTED TIME: {formatTime(values[0])}
         </div>
+        <div className="text-center mt-4 text-lg">COUNTDOWN: {countdown}</div>
       </div>
     </div>
   );
